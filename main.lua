7994d6f7ba06651d5489323f60bb03b7f54a39ac
Colin Pham <SettingFire666@gmail.com>
2025-02-28 01:06:14 -0800

    First Function

---
+++ main.lua
@@ -0,0 +1,15 @@
+local sm = require("steamodded")
+
+MOD_NAME = "KeyblePack"
+MOD_ID = "KeyblePack"
+MOD_AUTHOR = "Keyble"
+MOD_DESCRIPTION = "A Balatro Expansion Intended For Me and My Friends"
+BADGE_COLOUR = "6AE4FF"
+DISPLAY_NAME = "KeyblePack"
+PREFIX = "KeyP"
+MOD_VERSION = "0.0.1"
+-- DEPENDENCIES: [Steamodded>=1.0.0~ALPHA-1406b]
+
+function onGameStart()
+	print(MOD_NAME + " v" + MOD_VERSION + " has been loaded! HOORAY!")
+end
